cc34f31e8b0fda21501a74366b7c8eaecf336447
hovelopin <kimhojin3714@naver.com>
2023-05-08 22:55:02 +0900

    폴더 변경

---
+++ js/fc/greedy/b1946(신입 사원).js
@@ -0,0 +1,8 @@
+// 시작 (23시 30분) => 끝 ()
+const inputs = require("fs").readFileSync("./js/dev/stdin").toString().trim().split("\n");
+
+console.log('inputs' , inputs);
+
+const solution = (inputs) => {};
+
+console.log(solution(inputs));
