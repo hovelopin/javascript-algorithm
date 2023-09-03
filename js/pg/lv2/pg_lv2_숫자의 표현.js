154a97eddfee584f8b54c7b3d01c03ed76a78882
hovelopin <kimhojin3714@naver.com>
2023-09-03 17:55:38 +0900

    PG 9/3

--- js/pg/lv2/pg_lv2_숫자의 표현.js
+++ js/pg/lv2/pg_lv2_숫자의 표현.js
@@ -1,21 +1,27 @@
-// 시작 (23시 25분) => 끝 (23시 38분) => 완전 탐색 풀이 시간 초과 ( 테케 전부 통과 , 효율성 1개 실패 )
+// 17시 40분 ( 시작 ) => 17시 55분 ( 끝 )
+
 function solution(n) {
-    let answer = 0;
-    for(let i = 1 ; i<=n ; i++){
-      let sum = i;  
-      for(let j = i+1 ; j<=n; j++){
-        sum += j;
-        if(sum > n) {
-          break;
-        }
-        if(sum === n){
-          answer++;
-          break;
-        }
-        
+  let answer = 0;
+  let half = parseInt(n/2) + 1;
+
+  for(let i = 1 ;i <= n; i++){
+    let count = i;
+
+    for(let j = i+1; j <= half; j++){
+      count += j;
+
+      if(count > n){
+        break;
+      }
+
+      if(count === n){
+        answer ++;
+        break;
       }
     }
-    return answer+1;
+  }
+
+  return answer + 1;
 }
 
 console.log("정답 : ", solution(15));
